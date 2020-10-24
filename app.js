18c2fd477ebb39a37ddcc28924820d2a333cf2e8
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-10-23 21:52:20 -0700

    Adding animation effect

---
+++ app.js
@@ -0,0 +1,9 @@
+// Animation 
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+// Moving animation event
+container.addEventListener('mousemove', (e) => {
+    console.log(e.pageX, e.pageY);
+    let xAxis = window.innerWidth / 2
+})
